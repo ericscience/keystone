0a86c99a76e7eb6c9c413ba00a1c3718ed5487a5
Eric Garcia <eric.garcia@gmail.com>
2015-12-21 17:26:59 -0800

    update node-sass-middleware

--- setup.sh
+++ setup.sh
@@ -10,8 +10,7 @@ git@github.com:ericscience/keystone.git
 
 curl https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
 
-npm install -g gulp
-npm install -g bower
+npm install -g forever
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
